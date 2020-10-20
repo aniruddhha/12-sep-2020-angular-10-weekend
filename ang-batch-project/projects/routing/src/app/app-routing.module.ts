6cc22cd909c5c3fa56e3270c6e05825df57bb063
aniruddha <aniruddha.kudalkar@gmail.com>
2020-10-20 18:30:21 +0530

    routers: understanding and setup

---
+++ ang-batch-project/projects/routing/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { ChatsComponent } from './chats/chats.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'chats', component: ChatsComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
