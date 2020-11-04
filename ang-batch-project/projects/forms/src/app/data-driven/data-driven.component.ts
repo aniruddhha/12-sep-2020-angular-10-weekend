b0ae1dc93111eac7e651b531920f012bff65cec8
aniruddha <aniruddha.kudalkar@gmail.com>
2020-11-04 09:04:36 +0530

    forms : reactive forms : setup

--- ang-batch-project/projects/forms/src/app/data-driven/data-driven.component.ts
+++ ang-batch-project/projects/forms/src/app/data-driven/data-driven.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-data-driven',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataDrivenComponent implements OnInit {
 
-  constructor() { }
+  fg: FormGroup
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.fg = this.fb.group({
+      userInfo: this.fb.group({
+        yourName: this.fb.control(''),
+        company: this.fb.control(''),
+        mobile: this.fb.control(''),
+        email: this.fb.control('')
+      }),
+      productInfo: this.fb.group({
+        product: this.fb.control(''),
+        qty: this.fb.control(''),
+        price: this.fb.control(''),
+        tax: this.fb.control('')
+      }),
+      products: this.fb.array([])
+    })
   }
 
+  onQuotation() {
+    console.log(this.fg.value)
+  }
 }
